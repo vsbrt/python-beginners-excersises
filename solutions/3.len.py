91247d5610f36d5820d3c89051220f5b605f4ae0
vsb rt <vsb rt>
2016-07-31 12:08:59 +0530

    final version  3.len.py

--- solutions/3.len.py
+++ solutions/3.len.py
@@ -1,16 +1,9 @@
-ip = raw_input("Enter a string: ")
+ip = raw_input("Enter a string: ")				#taking input from the user
 
-def string_length(s):    
-    if s == '': return 0
-    return 1 + string_length(s[1:])
+def len_str(s):    								#function defined
+    if s == '': 
+	return 0									#comparing if the input given by user is empty
 	
-print'The lenth of the string is: ',string_length(ip)
-
-'''def len_str(a):
-	i=0
+    return 1 + len_str(s[1:])					#if input is non zero, increment up-to length of sting along with spaces and return value
 	
-	for letter in a:
-		i+=1
-		return i
-
-print'The lenth of the string is: ',len_str(ip)'''
+print'The lenth of the string is: ',len_str(ip)	#printing output
