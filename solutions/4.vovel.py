f61b3ff3f7f3a69e4a71053eca794a030e09f22a
vsb rt <vsb rt>
2016-07-31 13:41:33 +0530

    4th program almost minor issues to be resolved

--- solutions/4.vovel.py
+++ solutions/4.vovel.py
@@ -1,10 +1,15 @@
-import re															#regual expressions operations
+import re																#regular expressions operations
 
-input_str = raw_input("Please enter a single alphabet only: ")		#taking input from user
-if not re.match("^[a-z]*$", input_str):								#checking input for only alphabet
-    print "Error! Only letters a-z allowed!"
+input_str = raw_input("Please enter a single alphabet only: ")			#taking input from user
+if not re.match("^[a-z]*$", input_str):									#checking input for only alphabet
+    print "Error! Only letters a-z allowed!, no numbers ad special characters"
     sys.exit()
-elif len(input_str) > 1:											#limiting input to single alphabet
+elif len(input_str) > 1:												#limiting input to single alphabet
     print "Error! Only 1 character allowed!"
     sys.exit()
 
+if input_str in ('a', 'e', 'i', 'o', 'u'):								#Checking if input is vowel or constant
+        print "The given alphabet is vowel"
+		
+else:
+        print "The given alphabet is consonant"
