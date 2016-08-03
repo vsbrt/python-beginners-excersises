2c6673e9147b5912d760b8d39b3957863e2702e1
vsbrt <vsbrt@hotmail.com>
2016-08-03 21:52:49 +0530

    started  codo for fifth program

---
+++ solutions/5.translate.py
@@ -0,0 +1,13 @@
+
+def double_mult(s):
+    return ''.join([x*2 for x in s])
+
+def consonant(c):
+    if input_str in ('a', 'e', 'i', 'o', 'u'):								
+        
+		
+else:
+        
+
+def translate()
+
