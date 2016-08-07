dcd4bfc1ebe020f9bed545337a015821d70426d5
vsb rt <vsb rt>
2016-08-07 19:20:14 +0530

    alpha code 1

--- solutions/5.translate.py
+++ solutions/5.translate.py
@@ -1,7 +1,7 @@
 
 def double_mult(s):
 	vowels = 'aeiou'
-    return ''.join(l for l if l in vowels else l + 'o' + lin s)        
+    return ''.join(l for l if l in vowels else l + 'o' + lin input_str)        
 
 def translate(input_str):
 	input_str = raw_input("Please enter word to be Translated: ")
