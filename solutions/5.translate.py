43e83fb41ee4068de9993a24bcd258c0b2bee7c4
vsb rt <vsb rt>
2016-08-07 19:22:45 +0530

    revoked the entire code

--- solutions/5.translate.py
+++ solutions/5.translate.py
@@ -1,4 +1,4 @@
-
+'''
 def double_mult(s):
 	vowels = 'aeiou'
     return ''.join(l for l if l in vowels else l + 'o' + lin input_str)        
@@ -7,4 +7,10 @@ def translate(input_str):
 	input_str = raw_input("Please enter word to be Translated: ")
 	double_mult(input_str)
 	
+'''
 
+def translate(s):
+  consonants = 'bcdfghjklmnpqrstvwxz'
+  return ''.join(l + 'o' + l if l in consonants else l for l in s)
+
+print(translate("this is fun"))
