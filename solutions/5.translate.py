3e80deb74b9ebc0ea517f04b6b9fafdc7b297414
vsb rt <vsb rt>
2016-08-07 19:47:42 +0530

    final version

--- solutions/5.translate.py
+++ solutions/5.translate.py
@@ -1,19 +1,6 @@
-'''
-def double_mult(s):
-	vowels = 'aeiou'
-    return ''.join(l for l if l in vowels else l + 'o' + lin input_str)        
+def translate(string):
+	vowels = 'aeiou'														#Defined vowels
+	return ''.join(l + 'o' + l if l not in vowels else l for l in string)	#comparing string for vowels and consonants and adding an 'o' in between every consonant   
 
-def translate(input_str):
-	input_str = raw_input("Please enter word to be Translated: ")
-	double_mult(input_str)
-	
-'''
-
-def translate(s):
-	vowels = 'aeiou'
-	return ''.join(l + 'o' + l if l not in vowels else l for l in s)
-  #consonants = 'bcdfghjklmnpqrstvwxz'
-  #return ''.join(l + 'o' + l if l in consonants else l for l in s)
-
-input_str = raw_input("Please enter a word to be translated: ")
+input_str = raw_input("Please enter a word to be translated: ")				#taking input from user
 print(translate(input_str))
