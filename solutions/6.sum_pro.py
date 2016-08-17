1d86ae7cc48abbf83cb84dba4a04547b1c353857
vsb rt <vsb rt>
2016-08-17 17:21:55 +0530

    program 6 initial vrsion

---
+++ solutions/6.sum_pro.py
@@ -0,0 +1,12 @@
+def sum(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+
+def multiply(numbers):
+    total = 1
+    for number in numbers:
+        total = total * number
+    return total
