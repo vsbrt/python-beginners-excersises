904312597d6bed1b0e85df5bc41a7c7979187e8d
vsb rt <vsb rt>
2016-08-17 17:31:25 +0530

    program 6 initial vrsion

--- solutions/6.sum_pro.py
+++ solutions/6.sum_pro.py
@@ -1,12 +1,15 @@
+numbers = input("Please enter a number: ")
+
 def sum(numbers):
     total = 0
     for number in numbers:
         total += number
     return total
-
+	print(numbers)
 
 def multiply(numbers):
     total = 1
     for number in numbers:
         total = total * number
     return total
+	print(numbers)
