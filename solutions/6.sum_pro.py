696c6e1b88908ee40021a7861acc8ef1e69b887e
vsb rt <vsb rt>
2016-09-17 21:32:35 +0530

    working with fixed inputs

--- solutions/6.sum_pro.py
+++ solutions/6.sum_pro.py
@@ -19,3 +19,21 @@ if _name_ == "_main_":
 
 print sum(numbers)	
 print multiply(numbers)'''
+
+def sum(numbers):
+    total = 0
+    for number in numbers:
+        total += number
+    return total
+
+
+def multiply(numbers):
+    total = 1
+    for number in numbers:
+        total = total * number
+    return total
+
+
+if __name__ == "__main__":
+    print sum([1, 2, 3, 4])
+    print multiply([1, 2, 3, 4])
