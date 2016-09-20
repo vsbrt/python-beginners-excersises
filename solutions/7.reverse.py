e35f9e622f0550cf8ab2ee9af9fd33bf9de46b39
vsb rt <vsb rt>
2016-09-20 20:36:37 +0530

    Program 7 final stage

--- solutions/7.reverse.py
+++ solutions/7.reverse.py
@@ -1,26 +1,8 @@
-'''def reverse(string):
-    result = []
-    for word in string.split()[::-1]:
-        result.append(word[::-1])
-    return " ".join(result)
- 
-string = input("Enter the string to reverse ")
-
-reverse(string)
-
-
-print reverse("I am testing")'''
-
-
-
-
 def reverse(string):
     result = []
     for word in string.split()[::-1]:
         result.append(word[::-1])
     return " ".join(result)
  
-string = raw_input("Enter the string to reverse ")
-print(reverse(string))
-
-
+input_string = raw_input("Enter the string to be reversed: ")
+print(reverse(input_string))
