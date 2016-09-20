cae7f3d204d9645ef6388de28de2a437394f92ad
vsb rt <vsb rt>
2016-09-20 20:28:18 +0530

    started working in 7th program

--- solutions/7.reverse.py
+++ solutions/7.reverse.py
@@ -1,3 +1,19 @@
+'''def reverse(string):
+    result = []
+    for word in string.split()[::-1]:
+        result.append(word[::-1])
+    return " ".join(result)
+ 
+string = input("Enter the string to reverse ")
+
+reverse(string)
+
+
+print reverse("I am testing")'''
+
+
+
+
 def reverse(string):
     result = []
     for word in string.split()[::-1]:
