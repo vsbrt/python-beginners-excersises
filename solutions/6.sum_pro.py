ae2c2c746789e8f1d9348300c0607c0e4c43ab67
vsb rt <vsb rt>
2016-09-23 20:00:00 +0530

    program 6 final

--- solutions/6.sum_pro.py
+++ solutions/6.sum_pro.py
@@ -1,25 +1,3 @@
-'''numbers = input("Please enter a number: ")
-
-def sum(numbers):
-    total = 0
-    for number in numbers:
-        total += number
-    return total
-
-
-def multiply(numbers):
-    total = 1
-    for number in numbers:
-        total = total * number
-    return total
-
-if _name_ == "_main_":	
-	print sum([1, 2, 3, 4])
-	print multiply([1, 2, 3, 4])
-
-print sum(numbers)	
-print multiply(numbers)'''
-
 def sum(numbers):
     total = 0
     for number in numbers:
